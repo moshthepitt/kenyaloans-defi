64923dfbca2406ad437600f24d56e9b67f37f384
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-02-18 00:01:56 +0300

    Add constants module

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const WALLET_PROVIDER_URL = 'https://www.sollet.io';
+export const TESTNET = 'https://testnet.solana.com';
+export const WALLET = 'wallet';
+export const CONNECT = 'connect';
+export const DISCONNECT = 'disconnect';
