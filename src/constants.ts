7046778fac4bc725539d104dbb6e93dda026f90f
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-02-19 23:41:46 +0300

    Add more constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
 export const WALLET_PROVIDER_URL = 'https://www.sollet.io';
 export const TESTNET = 'https://testnet.solana.com';
+export const LOCALNET = 'http://localhost:8899';
 export const WALLET = 'wallet';
 export const CONNECT = 'connect';
 export const DISCONNECT = 'disconnect';
+export const SINGLE_GOSSIP = 'singleGossip';
+export const LE = 'le';
+export const BASE10 = 10;
