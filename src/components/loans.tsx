e1aa571a09d4d1a0f0c52684e44f2cffeecaa554
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-02-25 21:49:38 +0300

    Get connection from global state

--- src/components/loans.tsx
+++ src/components/loans.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Spinner, HTMLTable } from '@blueprintjs/core';
 import { useQuery } from 'react-query';
-import type { Connection } from '@solana/web3.js';
 import { getLoanAccounts } from '../utils/api';
 import type { LoanAccount } from '../utils/api';
-import { LOAN } from '../constants';
+import { useGlobalState } from '../utils/state';
+import { CONNECTION, LOAN } from '../constants';
 
 interface Props {
-  connection: Connection;
   loanProgramId: string;
 }
 
 const Loans = (props: Props): JSX.Element => {
-  const loanQuery = async () => getLoanAccounts(props);
+  const [connection] = useGlobalState(CONNECTION);
+  const loanQuery = async () => getLoanAccounts({ ...props, connection });
   const { isLoading, error, data } = useQuery(LOAN, loanQuery);
 
   if (isLoading) {
@@ -25,19 +25,21 @@ const Loans = (props: Props): JSX.Element => {
 
   return (
     <HTMLTable>
-      <tr>
-        <th>id</th>
-        <th>amount</th>
-        <th>status</th>
-      </tr>
-      {data &&
-        data.map((loan: LoanAccount) => (
-          <tr key={loan.id}>
-            <td>{loan.id}</td>
-            <td>{loan.expectedAmount}</td>
-            <td>{loan.status}</td>
-          </tr>
-        ))}
+      <tbody>
+        <tr>
+          <th>id</th>
+          <th>amount</th>
+          <th>status</th>
+        </tr>
+        {data &&
+          data.map((loan: LoanAccount) => (
+            <tr key={loan.id}>
+              <td>{loan.id}</td>
+              <td>{loan.expectedAmount}</td>
+              <td>{loan.status}</td>
+            </tr>
+          ))}
+      </tbody>
     </HTMLTable>
   );
 };
