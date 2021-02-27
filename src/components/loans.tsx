0162322ccea3d24b9bb9ce5c79792cf16b889626
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-02-27 14:17:23 +0300

    Add more loan data + filtering by initializer

--- src/components/loans.tsx
+++ src/components/loans.tsx
@@ -8,9 +8,11 @@ import { CONNECTION, LOAN } from '../constants';
 
 interface Props {
   loanProgramId: string;
+  initializer?: string;
 }
 
 const Loans = (props: Props): JSX.Element => {
+  const { initializer } = props;
   const [connection] = useGlobalState(CONNECTION);
   const loanQuery = async () => getLoanAccounts({ ...props, connection });
   const { isLoading, error, data } = useQuery(LOAN, loanQuery);
@@ -23,22 +25,47 @@ const Loans = (props: Props): JSX.Element => {
     return <span>Error...</span>;
   }
 
+  let loans: LoanAccount[] = data || [];
+  if (initializer) {
+    loans = loans.filter((item) => item.initializerPubkey === initializer);
+  }
+
+  const getStatusForUI = (status: number) => {
+    switch (status) {
+      case 0:
+        return 'Pending';
+      case 1:
+        return 'Initialized';
+      case 2:
+        return 'Guaranteed';
+      case 3:
+        return 'Accepted';
+      case 4:
+        return 'Repaid';
+      default:
+        return 'Invalid';
+    }
+  };
+
   return (
     <HTMLTable>
       <tbody>
         <tr>
-          <th>id</th>
-          <th>amount</th>
+          <th>ID</th>
+          <th>Amount</th>
+          <th>Duration (hours)</th>
+          <th>APR</th>
           <th>status</th>
         </tr>
-        {data &&
-          data.map((loan: LoanAccount) => (
-            <tr key={loan.id}>
-              <td>{loan.id}</td>
-              <td>{loan.expectedAmount}</td>
-              <td>{loan.status}</td>
-            </tr>
-          ))}
+        {loans.map((loan) => (
+          <tr key={loan.id}>
+            <td>{loan.id}</td>
+            <td>{loan.expectedAmount}</td>
+            <td>{loan.duration}</td>
+            <td>{loan.interestRate}%</td>
+            <td>{getStatusForUI(loan.status)}</td>
+          </tr>
+        ))}
       </tbody>
     </HTMLTable>
   );
