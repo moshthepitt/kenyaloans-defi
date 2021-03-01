931bd5ba9652679b9fce801bab815b016a6e4239
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-03-01 23:47:59 +0300

    Use devnet as default

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const PROGRAM_ID: string | undefined = process.env.REACT_APP_PROGRAM_ID;
 export const SOLANA_NETWORK_URL: string =
-  process.env.REACT_APP_SOLANA_NETWORK_URL || 'http://localhost:8899';
+  process.env.REACT_APP_SOLANA_NETWORK_URL || 'https://devnet.solana.com';
